1025e6596509f6f23267f5c3845755d1acf97c2f
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2025-04-03 22:20:02 -0300

    Contador feito, exercício das frases motivacionais iniciado.

---
+++ src/marco/exercicios/ex4/ContadorDePalavras.kt
@@ -0,0 +1,15 @@
+package marco.exercicios.ex4
+
+fun main(){
+    println("\nContador de Termos 2000" +
+            "\n\nQual é o texto?")
+    val texto = readln()
+
+    println("\nQual é o termo que deseja pesquisar no texto?")
+    val termo = readln()
+
+    val pesquisador = Regex("\\b${Regex.escape(termo)}\\b")
+    val contagem = pesquisador.findAll(texto).count()
+
+    println("\nO termo \"$termo\" se repete $contagem veze(s).")
+}
---
+++ src/marco/exercicios/ex5/FrasesMotivacionaisPorHora.kt
@@ -0,0 +1,2 @@
+package marco.exercicios.ex5
+
