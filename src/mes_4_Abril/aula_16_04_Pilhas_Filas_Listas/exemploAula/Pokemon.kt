983e087bbb2fe25fe1ee9c9e257ef66ffa14d5fb
João Pedro G U <41847866+Charmandercharmandesco@users.noreply.github.com>
2025-04-16 22:12:13 -0300

    Exemplos da aula

---
+++ src/mes_4_Abril/aula_16_04_Pilhas_Filas_Listas/exemploAula/Pokemon.kt
@@ -0,0 +1,49 @@
+package mes_4_Abril.aula_16_04_Pilhas_Filas_Listas.exemploAula
+
+fun main() {
+    val bulbasauro = mapOf(
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "VENENO" to  listOf("FADA, PLANTA")
+    )
+
+    val ivisaurro = mapOf(
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "VENENO" to  listOf("FADA, PLANTA")
+    )
+
+    val venusauro = mapOf(
+        "GRAMA" to  listOf("ÁGUA", "TERRA", "PEDRA"),
+        "VENENO" to  listOf("FADA, PLANTA")
+    )
+
+    val charmander = mapOf(
+        "FOGO" to listOf("PLANTA", "AÇO", "GELO", "INSETO")
+    )
+
+    val charmeleon = mapOf(
+        "FOGO" to listOf("PLANTA", "AÇO", "GELO", "INSETO")
+    )
+
+    val charizard = mapOf(
+        "FOGO" to listOf("PLANTA", "AÇO", "GELO", "INSETO"),
+        "VOADOR" to listOf("PLANTA", "INSETO", "LUTADOR")
+    )
+
+    val squirtle = mapOf(
+        "ÁGUA" to listOf("FOGO", "TERRA", "PEDRA")
+    )
+
+    val warturtle = mapOf(
+        "ÁGUA" to listOf("FOGO", "TERRA", "PEDRA")
+    )
+
+    val blastoise = mapOf(
+        "ÁGUA" to listOf("FOGO", "TERRA", "PEDRA")
+    )
+
+    val caterpie = mapOf(
+        "INSETO" to listOf("PLANTA", "SOMBRIO", "PSIQUICO")
+    )
+
+
+}
---
+++ src/mes_4_Abril/aula_16_04_Pilhas_Filas_Listas/exercicios/exPilhaDeProdutos/Produto.kt
@@ -0,0 +1,5 @@
+package mes_4_Abril.aula_16_04_Pilhas_Filas_Listas.exercicios.exPilhaDeProdutos
+
+class Produto {
+
+}
