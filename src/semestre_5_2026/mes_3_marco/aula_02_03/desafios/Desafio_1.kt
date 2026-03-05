ada4c2a24cf70426da38c8242f508d4c63759784
joaopug <charmandercharmandesco@gmail.com>
2026-03-05 14:56:18 -0300

    Agora usa when 😎😎

--- src/semestre_5_2026/mes_3_marco/aula_02_03/desafios/Desafio_1.kt
+++ src/semestre_5_2026/mes_3_marco/aula_02_03/desafios/Desafio_1.kt
@@ -1,23 +1,20 @@
 package semestre_5_2026.mes_3_marco.aula_02_03.desafios
 
-fun main(){
+fun main() {
     println("Digite a conta quer fazer. (Somente uma operação):")
     val operacao = readln().replace(" ", "")
 
-    val num1 = operacao.substring(0,1).toDouble()
-    val operador = operacao.substring(1,2)
-    val num2 = operacao.substring(2,3).toDouble()
+    val num1 = operacao.substring(0, 1).toDouble()
+    val operador = operacao.substring(1, 2)
+    val num2 = operacao.substring(2, 3).toDouble()
 
     var operacaoFeita = 0.0
 
-    if (operador == "+"){
-        operacaoFeita = num1+num2
-    } else if (operador == "-"){
-        operacaoFeita = num1 - num2
-    } else if (operador == "*"){
-        operacaoFeita = num1 * num2
-    } else if (operador == "/") {
-        operacaoFeita = num1 / num2
+    when (operador) {
+        "+" -> operacaoFeita = num1 + num2
+        "-" -> operacaoFeita = num1 - num2
+        "*" -> operacaoFeita = num1 * num2
+        "/" -> operacaoFeita = num1 / num2
     }
 
     println(operacaoFeita)
